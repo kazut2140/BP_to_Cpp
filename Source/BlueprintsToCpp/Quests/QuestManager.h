ad937b46784c06875ca68cd56acedf5dbfd2657f
kazut2140 <k.tsuji@bitgroove.jp>
2023-05-26 16:16:13 +0900

    デリゲートの宣言

--- Source/BlueprintsToCpp/Quests/QuestManager.h
+++ Source/BlueprintsToCpp/Quests/QuestManager.h
@@ -7,6 +7,8 @@
 #include "QuestInfo.h"
 #include "QuestManager.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCompletedQuestSignature, int32, Index);
+
 UCLASS()
 class BLUEPRINTSTOCPP_API AQuestManager : public AActor
 {
@@ -25,6 +27,9 @@ public:
 
 	UFUNCTION(BlueprintPure, BlueprintImplementableEvent)
 	bool IsActiveQuest(FName Name) const;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FCompletedQuestSignature CompletedQuest;
 
 protected:
 	// Called when the game starts or when spawned
